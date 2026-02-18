2f12ff5ac981d19944b3ae6e23d238d87b6d0366
navnath13 <navnathdoke2222@gmail.com>
2026-02-18 18:41:00 +0530

    change to key and pair value

--- src/main/java/com/githubaction/controller/TestController.java
+++ src/main/java/com/githubaction/controller/TestController.java
@@ -1,6 +1,10 @@
 package com.githubaction.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -10,8 +14,11 @@ public class TestController {
    
 
      @GetMapping
-    public String message(){
-        return "github action working properly v5";
+    public Map message(){
+        Map<String,String> obj=new HashMap<>();
+        obj.put("Name", "Navanth doke");
+        obj.put("status", "sucesss");
+        return obj;
     }
 
 }
